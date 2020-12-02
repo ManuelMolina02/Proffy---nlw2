12c7a4d9bb93542ac26d281ba45bd47217df390a
manuelmolina <manuelanmolina@gmail.com>
2020-12-02 11:29:13 -0300

    fim da 3ª aula - correçao addField

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,13 +1,21 @@
+// -----------> Duplicar elemento HTML <-----------
+
 // procurar o botao
 document.querySelector("#add-time")
-
-
     // quando clicar no botao
     .addEventListener('click', cloneField)
-
 // executar ação
 function cloneField() {
-    document.querySelector('.schedule-item').cloneNode(true)
     // duplicar os campos, mas que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
 
-    // colocar na página
+    //pegar os campos, que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+    //para cada campo limpar, que campos?
+    fields.forEach(function(field){
+        //pegar o field do momento e limpar ele
+        field.value = ""
+    })
+    // colocar na página: onde?
+    document.querySelector("#schedule-items").appendChild(newFieldContainer)
+}
